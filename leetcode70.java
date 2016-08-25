f12f20fcdaa3b537fa4a509c9d51e63c047f568f
gaohongbin <1501210900@pku.edu.cn>
2016-08-25 23:41:30 +0800

    leetcode70

---
+++ leetcode70.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int climbStairs(int n) {
+         if(n==1)
+			 return 1;
+		 if(n==2)
+			 return 2;
+	        int[] nums=new int[n+1];
+	        nums[0]=1;
+	        nums[1]=1;
+	        nums[2]=2;
+	        for(int i=3;i<=n;i++){
+	        	nums[i]=nums[i-1]+nums[i-2];
+	        }
+	        return nums[n];
+    }
+}
