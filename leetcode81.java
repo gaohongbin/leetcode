2a3b07f089ba77ffc098f5cfbc7d5198b40acb1e
gaohongbin <1501210900@pku.edu.cn>
2016-08-30 23:53:43 +0800

    leetcode81

---
+++ leetcode81.java
@@ -0,0 +1,15 @@
+/**
+* 什么都不管，直接遍历，结果时间还是最快的。
+*/
+
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        if(nums == null || nums.length == 0)
+         return false;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == target)
+             return true;
+        }
+        return false;
+    }
+}
